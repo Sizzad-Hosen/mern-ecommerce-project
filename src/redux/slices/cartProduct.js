54dfe06517a110f137a3c903d5e0f2c6cc4e6b9d
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-04-21 22:10:04 +0600

    Done fix code isssu

--- src/redux/slices/cartProduct.js
+++ src/redux/slices/cartProduct.js
@@ -1,21 +1,20 @@
-const { createSlice } = require("@reduxjs/toolkit")
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    cart:[]
-}
-
+    cart: []
+};
 
 const cartSlice = createSlice({
-    name:"cartItem",
-    initialState:initialState,
-    reducers:{
-        handleAddToCart:(state,action)=>{
-            state.cart = [...action.payload]
+    name: "cartItem",
+    initialState,
+    reducers: {
+        handleAddToCart: (state, action) => {
+            // Add the new item to the existing cart array
+            state.cart.push(action.payload);
         }
     }
-})
+});
 
-export const {handleAddToCart} = cartSlice.actions;
+export const { handleAddToCart } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
